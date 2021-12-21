7bf4aec4fffffc2b04375d860ea0305e629c1169
fabianmolinab <molinafabian11b@gmail.com>
2021-12-21 03:07:20 -0500

    Configuración para test con typescript, y se agrego el script del linter

---
+++ src/test/auth/authReducer.test.ts
@@ -0,0 +1,14 @@
+describe('Pruebas en authReducer', () => {
+
+  test('debe de retornar el estado por defecto', () => {
+
+  })
+
+  test('debe de autenticar y colocar el name del usuario', () => {
+
+  })
+
+  test('debe de borrar el name del usuario y logged en false', () => {
+
+  })
+})
